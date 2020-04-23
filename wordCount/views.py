43f77ac89f49fd3b9850a0d996a53002a615c244
Md Zahangir Alam <Zahangir@APLAP60_Zahangir.dse.com.bd>
2020-04-23 20:14:58 +0600

    This is my first commit in Word Count Porject

---
+++ wordCount/views.py
@@ -0,0 +1,32 @@
+from django.shortcuts import render
+
+
+def home(request):
+    # return HttpResponse("Welcome to Django Word Count Home Page!!..")
+    return render(request, 'homepage.html')
+
+
+def count(request):
+    text = request.GET['text']
+    textList = text.split()
+    length = len(textList)
+    textDict = {}
+
+    print("LL: {}".format(type(textDict)))
+    print("dd: {}".format(type(textDict)))
+
+    for t in textList:
+        # print(t, end=", ")
+        if t in textDict:
+            textDict[t] += 1
+        else:
+            textDict[t] = 1
+
+    # textDict = {k: v for k, v in sorted(textDict.items(), key=lambda x: x[1], reverse=True)}
+    textDict = sorted(textDict.items(), key=lambda x: x[1], reverse=True)
+    print(type(textDict))
+    return render(request, 'wordCount.html', {'text': text, 'length': length, 'textDict':textDict})
+
+
+def about(request):
+    return render(request, 'about.html')
